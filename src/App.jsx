57d787549343c2b7032192dd750015594c3f8d90
Adarsh Modi <adarsh.modi@sprinklr.com>
2023-06-27 10:42:51 +0530

    again changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,25 @@ import './App.css';
 
 
 function App() {
-  // const [showGallery, setShowGallery] = useState(false);
+  const [showGallery, setShowGallery] = useState(false);
 
   const handleClick = () => {
     // Dynamically import the ImageGallery component
-    // import('./ImageGallery').then((module) => {
-    //   setShowGallery((prev) => (!prev));
-    // });
+    import('./ImageGallery').then((module) => {
+      setShowGallery((prev) => (!prev));
+    });
   };
 
   // const ImageGallery = lazy(() => import('./ImageGallery'));
   return (
     <div className="app">
       <h1>Lazy Image Gallery</h1>
-      {/* <button onClick={handleClick} className='custom-button'>Load Gallery</button>
+      <button onClick={handleClick} className='custom-button'>Load Gallery</button>
       {showGallery && (
         <Suspense fallback={<div>Loading...</div>}>
           <ImageGallery />
         </Suspense>
-      )} */}
+      )}
     </div>
   );
 }
