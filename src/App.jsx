4a88bbbcce10c7b9dc27ee8196fb2396fc0074be
Adarsh Modi <adarsh.modi@sprinklr.com>
2023-06-27 19:52:54 +0530

    changed dist

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,12 @@ function App() {
   return (
     <div className="App">
       <h1>Lazy Image Gallery</h1>
-      <button onClick={handleClick} className='custom-button'>Load Gallery</button>
+      {/* <button onClick={handleClick} className='custom-button'>Load Gallery</button>
       {showGallery && (
         <Suspense fallback={<div>Loading...</div>}>
           <ImageGallery />
         </Suspense>
-      )}
+      )} */}
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle2.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
